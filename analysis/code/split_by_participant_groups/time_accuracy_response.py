19290fcc5218fd91491a7c0c7f9127c7740f2d56
Zehua Zeng <fiona.zehua.zeng@gmail.com>
2021-02-22 20:21:47 -0500

    accuracy split by groups

--- analysis/code/split_by_participant_groups/time_accuracy_response.py
+++ analysis/code/split_by_participant_groups/time_accuracy_response.py
@@ -34,52 +34,15 @@ response_to_score_dict = {
 path_to_json = '../../logs/'
 
 ## completion time
-jsons_data = pd.DataFrame(columns=['participant_id',
-                                   'participant_group',
-                                   'dataset',
-                                   'oracle',
-                                   'search',
-                                   'task',
-                                   'completion_time'])
-
-json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('_logs.json')]
-
-for index, js in enumerate(json_files):
-    with open(os.path.join(path_to_json, js)) as json_file:
-        json_text = json.load(json_file)
-
-        split_filename = js.split('_')
-
-        participant_id = split_filename[0]
-        participant_group = ''
-        if participant_id.startswith('pro'):
-            participant_group = 'professional'
-        elif participant_id.startswith('stu'):
-            participant_group = 'student'
-        experimental_setup = split_filename[1]
-        dataset = dataset_dict[experimental_setup[0]]
-        oracle = oracle_dict[experimental_setup[1]]
-        search = search_algorithm_dict[experimental_setup[2]]
-        task = task_dict[split_filename[2]]
-
-        completion_time = (json_text[-1]["Time"] - json_text[0]["Time"]) / 1000
-
-        row = [participant_id, participant_group, dataset, oracle, search, task, completion_time]
-        jsons_data.loc[index] = row
-    
-jsons_data.to_csv('completion_time.csv', index=False)
-
-
-## accuracy
 # jsons_data = pd.DataFrame(columns=['participant_id',
 #                                    'participant_group',
 #                                    'dataset',
 #                                    'oracle',
 #                                    'search',
 #                                    'task',
-#                                    'accuracy'])
+#                                    'completion_time'])
 
-# json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('_p1_answer.json')] + [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('_p2_answer.json')]
+# json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('_logs.json')]
 
 # for index, js in enumerate(json_files):
 #     with open(os.path.join(path_to_json, js)) as json_file:
@@ -99,36 +62,73 @@ jsons_data.to_csv('completion_time.csv', index=False)
 #         search = search_algorithm_dict[experimental_setup[2]]
 #         task = task_dict[split_filename[2]]
 
-#         accuracy = -1
+#         completion_time = (json_text[-1]["Time"] - json_text[0]["Time"]) / 1000
 
-#         if experimental_setup[0] == "a" and split_filename[2] == "p1":
-#             if json_text["answer"] == "contemporary_fiction":
-#                 accuracy = 1
-#             else:
-#                 accuracy = 0
-        
-#         if experimental_setup[0] == "a" and split_filename[2] == "p2":
-#             if json_text["answer"] == "151":
-#                 accuracy = 1
-#             else:
-#                 accuracy = 0
-        
-#         if experimental_setup[0] == "b" and split_filename[2] == "p1":
-#             if json_text["answer"] == "approach":
-#                 accuracy = 1
-#             else:
-#                 accuracy = 0
-        
-#         if experimental_setup[0] == "b" and split_filename[2] == "p2":
-#             if json_text["answer"] == "235":
-#                 accuracy = 1
-#             else:
-#                 accuracy = 0
-
-#         row = [participant_id, participant_group, dataset, oracle, search, task, accuracy]
+#         row = [participant_id, participant_group, dataset, oracle, search, task, completion_time]
 #         jsons_data.loc[index] = row
     
-# jsons_data.to_csv('accuracy.csv', index=False)
+# jsons_data.to_csv('completion_time.csv', index=False)
+
+
+## accuracy
+jsons_data = pd.DataFrame(columns=['participant_id',
+                                   'participant_group',
+                                   'dataset',
+                                   'oracle',
+                                   'search',
+                                   'task',
+                                   'accuracy'])
+
+json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('_p1_answer.json')] + [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('_p2_answer.json')]
+
+for index, js in enumerate(json_files):
+    with open(os.path.join(path_to_json, js)) as json_file:
+        json_text = json.load(json_file)
+
+        split_filename = js.split('_')
+
+        participant_id = split_filename[0]
+        participant_group = ''
+        if participant_id.startswith('pro'):
+            participant_group = 'professional'
+        elif participant_id.startswith('stu'):
+            participant_group = 'student'
+        experimental_setup = split_filename[1]
+        dataset = dataset_dict[experimental_setup[0]]
+        oracle = oracle_dict[experimental_setup[1]]
+        search = search_algorithm_dict[experimental_setup[2]]
+        task = task_dict[split_filename[2]]
+
+        accuracy = -1
+
+        if experimental_setup[0] == "a" and split_filename[2] == "p1":
+            if json_text["answer"] == "contemporary_fiction":
+                accuracy = 1
+            else:
+                accuracy = 0
+        
+        if experimental_setup[0] == "a" and split_filename[2] == "p2":
+            if json_text["answer"] == "151":
+                accuracy = 1
+            else:
+                accuracy = 0
+        
+        if experimental_setup[0] == "b" and split_filename[2] == "p1":
+            if json_text["answer"] == "approach":
+                accuracy = 1
+            else:
+                accuracy = 0
+        
+        if experimental_setup[0] == "b" and split_filename[2] == "p2":
+            if json_text["answer"] == "235":
+                accuracy = 1
+            else:
+                accuracy = 0
+
+        row = [participant_id, participant_group, dataset, oracle, search, task, accuracy]
+        jsons_data.loc[index] = row
+    
+jsons_data.to_csv('accuracy.csv', index=False)
 
 ## user response
 # jsons_data = pd.DataFrame(columns=['participant_id',
